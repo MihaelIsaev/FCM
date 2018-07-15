6fb3600a3d82eac37ee70df5dcde36cce78dd219
Shaun Hubbard <shaun@splife.com>
2018-07-15 00:31:01 -0400

    updated

--- Sources/FCM/FCMAPNSAPSObject.swift
+++ Sources/FCM/FCMAPNSAPSObject.swift
@@ -1,6 +1,6 @@
 // The following struct is based on
 // https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/generating_a_remote_notification
-public struct FCMAPNSPayload: Codable {
+public struct FCMAPNSAPSObject: Codable {
     /// The information for displaying an alert. A dictionary is recommended. If you specify a string, the alert displays your string as the body text.
     var alert: FCMAPNSAlertOrString?
     /// The number to display in a badge on your app’s icon. Specify 0 to remove the current badge, if any.
--- Sources/FCM/FCMApnsConfig.swift
+++ Sources/FCM/FCMApnsConfig.swift
@@ -1,27 +1,16 @@
 import Foundation
 
-public struct FCMApnsConfig: Codable {
+final public class FCMApnsConfig: Codable {
     ///HTTP request headers defined in Apple Push Notification Service. Refer to APNs request headers for supported headers, e.g. "apns-priority": "10".
     public var headers: [String: String] = [:]
     ///APNs payload as a JSON object, including both aps dictionary and custom payload. See Payload Key Reference. If present, it overrides google.firebase.fcm.v1.Notification.title and google.firebase.fcm.v1.Notification.body.
     public var payload: FCMAPNSPayload?
 
+
     /// Publically Initialize
     public init(headers: [String: String] = [:], payload: FCMAPNSPayload? = nil) {
         self.headers = headers
         self.payload = payload
-    }
-
-    enum CodingKeys: String, CodingKey {
-        case headers, payload
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encodeIfPresent(headers, forKey: .headers)
-        if let payload = payload {
-            try container.encode(ApsWrapper(aps: payload), forKey: .payload)
-        }
     }
 }
 
@@ -29,7 +18,9 @@ public struct FCMApnsConfig: Codable {
 // apns: {
 //   payload: {
 //     aps: {
+//     }
+//     CUSTOM_KEYS that will be stuck in the, so it must be open to extension
 
-private struct ApsWrapper: Codable {
-    let aps: FCMAPNSPayload
+public class FCMAPNSPayload: Codable {
+    var aps: FCMAPNSAPSObject?
 }
