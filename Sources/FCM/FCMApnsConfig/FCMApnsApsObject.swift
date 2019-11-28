e259817b82b73853acf39f32f3026be3201f8644
Frédéric Ruaudel <fred@h2g.io>
2019-11-28 11:04:04 +0100

    make the APNS data model properties accessible to ease test

--- Sources/FCM/FCMApnsConfig/FCMApnsApsObject.swift
+++ Sources/FCM/FCMApnsConfig/FCMApnsApsObject.swift
@@ -4,10 +4,10 @@ public class FCMApnsApsObject: Codable {
     /// The information for displaying an alert.
     /// A dictionary is recommended.
     /// If you specify a string, the alert displays your string as the body text.
-    var alert: FCMApnsAlertOrString?
+    public var alert: FCMApnsAlertOrString?
     
     /// The number to display in a badge on your app’s icon. Specify 0 to remove the current badge, if any.
-    var badge: Int?
+    public var badge: Int?
     
     /// The name of a sound file in your app’s main bundle
     /// or in the Library/Sounds folder of your app’s container directory.
@@ -16,27 +16,27 @@ public class FCMApnsApsObject: Codable {
     /// For critical alerts, use the sound dictionary instead.
     /// For information about how to prepare sounds, see UNNotificationSound.
     /// UNNotificationSound: https://developer.apple.com/documentation/usernotifications/unnotificationsound
-    var sound: String? // NOTE: Advanced Sounds are not implemented yet
+    public var sound: String? // NOTE: Advanced Sounds are not implemented yet
     
     /// An app-specific identifier for grouping related notifications.
     /// This value corresponds to the threadIdentifier property in the UNNotificationContent object.
-    var threadId: String?
+    public var threadId: String?
     
     /// The notification’s type.
     /// This string must correspond to the identifier
     /// of one of the UNNotificationCategory objects you register at launch time.
-    var category: String?
+    public var category: String?
     
     /// The background notification flag.
     /// To perform a silent background update,
     /// specify the value 1
     /// and don't include the alert, badge, or sound keys in your payload.
-    var contentAvailable: Int?
+    public var contentAvailable: Int?
     
     /// The notification service app extension flag.
     /// If the value is 1, the system passes the notification to your notification service app extension before delivery.
     /// Use your extension to modify the notification’s content.
-    var mutableContent: Int?
+    public var mutableContent: Int?
 
     enum CodingKeys: String, CodingKey {
         case alert
