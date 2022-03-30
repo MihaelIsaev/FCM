bc7a73287060548ab1442ae3d9d7fe6381d169ba
Brommko LLC <brommko@yahoo.com>
2022-03-30 11:44:36 +0200

    - upgrade notification

--- Sources/FCM/FCMNotification.swift
+++ Sources/FCM/FCMNotification.swift
@@ -1,15 +1,29 @@
 public struct FCMNotification: Codable {
     /// The notification's title.
-    var title: String
+    public var title: String
     
     /// The notification's body text.
-    var body: String
+    public var body: String
+    
+    public var badge: String?
+
+    public var clickAction: String?
+    
+    public var priority: String?
+    
+    public enum CodingKeys: String, CodingKey {
+        case title, body, badge, priority
+        case clickAction = "click-action"
+    }
     
     /// - parameters:
     ///     - title: The notification's title.
     ///     - body: The notification's body text.
-    public init(title: String, body: String) {
+    public init(title: String, body: String, badge: String? = nil, clickAction: String? = nil, priority: String? = "high") {
         self.title = title
         self.body = body
+        self.badge = badge
+        self.clickAction = clickAction
+        self.priority = priority
     }
 }
