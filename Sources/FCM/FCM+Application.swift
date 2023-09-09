2e04436b4ba972c5ac960d06e6a7e0d7b1fa2559
Alessandro Di Maio <alessandrodimaio99@gmail.com>
2023-09-09 13:36:45 +0200

    R removed useless retrieve by id on app.fcm, id is provided via .use and .client

--- Sources/FCM/FCM+Application.swift
+++ Sources/FCM/FCM+Application.swift
@@ -2,10 +2,6 @@ import Vapor
 
 extension Application {
     public var fcm: FCM.Storage {
-        fcm(.default)
-    }
-
-    public func fcm(_ id: FCM.ID) -> FCM.Storage {
         .init(application: self)
     }
 }
