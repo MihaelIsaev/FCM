c80239e6044d4b94e18249331506601dc43e0b2d
Nikola Paunović <nikola@applycolo.rs>
2024-03-12 21:50:44 +0100

    Add image url as string in options

--- Sources/FCM/FCMApnsConfig/FCMOptions.swift
+++ Sources/FCM/FCMApnsConfig/FCMOptions.swift
@@ -1,12 +1,16 @@
 public struct FCMOptions: Codable, Equatable, FCMOptionsProtocol {
     /// Label associated with the message's analytics data.
     public var analyticsLabel: String
+    /// Image to be shown in push messages
+    public var image: String
 
     enum CodingKeys: String, CodingKey {
         case analyticsLabel = "analytics_label"
+        case image = "image"
     }
 
     public init(analyticsLabel: String, image: String = "") {
         self.analyticsLabel = analyticsLabel
+        self.image = image
     }
 }
